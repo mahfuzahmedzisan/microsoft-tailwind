e6c393eb9a10d582fac077935491703b2e02a100
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-10-11 00:55:11 +0600

    Working Header Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,9 @@ module.exports = {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '3rem',
       },
     },
     extend: {
