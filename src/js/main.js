2b9b6ee7b30446b5d76453abba0987e3874dd484
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-10-11 19:47:46 +0600

    Create Mobile Menu

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const openMenuBtn = document.getElementById("open-menu-btn");
+const closeMenuBtn = document.getElementById("close-menu-btn");
+const menu = document.getElementById("menu");
+
+const dropDown = document.querySelectorAll(".drop-down");
+
+openMenuBtn.addEventListener("click", () => {
+   menu.classList.add("hidden")
+   if (menu.classList.contains("hidden")) {
+      openMenuBtn.classList.add("hidden")
+      closeMenuBtn.classList.remove("hidden")
+      menu.classList.remove("hidden")
+   }
+   else {
+      closeMenuBtn.classList.add("hidden")
+      openMenuBtn.classList.remove("hidden")
+      menu.classList.add("hidden")
+   }
+   dropDown.forEach((item) => {
+      item.classList.remove("after:rotate-180")
+      item.nextElementSibling.classList.add("hidden")
+   })
+});
+closeMenuBtn.addEventListener("click", () => {
+   if (menu.classList.contains("hidden")) {
+      openMenuBtn.classList.add("hidden")
+      closeMenuBtn.classList.remove("hidden")
+      menu.classList.remove("hidden")
+   }
+   else {
+      closeMenuBtn.classList.add("hidden")
+      openMenuBtn.classList.remove("hidden")
+      menu.classList.add("hidden")
+   }
+})
+dropDown.forEach((item) => {
+   item.addEventListener("click", () => {
+      item.classList.toggle("after:rotate-180")
+      item.nextElementSibling.classList.toggle("hidden")
+   })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*", "./src/**/*.{js,jsx,ts,tsx,html}"],
+  darkMode: 'selector',
   theme: {
     screens: {
       'xs': "480px",
@@ -17,10 +18,6 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '2rem',
-        xl: '2rem',
-        '2xl': '3rem',
       },
     },
     extend: {
